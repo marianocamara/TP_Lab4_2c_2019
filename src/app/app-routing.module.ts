c2deda8f7d55938627e049917b2ea0300784c57c
marianocamara <camaramariano@gmail.com>
2019-10-13 00:33:03 -0300

    route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {path: 'login' , component: LoginComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full'}
 ];
 
 @NgModule({
