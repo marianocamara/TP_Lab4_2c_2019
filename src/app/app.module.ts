4ff489b6ed12ab1690a6475c4f416867f3a5f8e8
marianocamara <camaramariano@gmail.com>
2019-10-24 19:22:56 -0300

    firebase & captcha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+import { AuthService } from './services/auth.service';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+
+var firebase = {
+  apiKey: "AIzaSyAC4t4yhrWOZUsaeKGLsksVbdM0CnTz6K8",
+  authDomain: "tp-2-comanda.firebaseapp.com",
+  databaseURL: "https://tp-2-comanda.firebaseio.com",
+  projectId: "tp-2-comanda",
+  storageBucket: "tp-2-comanda.appspot.com",
+  messagingSenderId: "1042369693807",
+  appId: "1:1042369693807:web:66ee92bd9520c686d0659a"
+};
+
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFontAwesomeModule
+    FormsModule,
+    AngularFontAwesomeModule,
+    AngularFireModule.initializeApp(firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,20 +7,74 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
+  
+  email = 'admin@admin.com';
+  pass= 'adminpass';
+  newEmail;
+  newPass;
+  newName;
+  isLoading = false;
+  
+  constructor(public authService:AuthService) { }
+  
+  @ViewChild('recaptcha', {static: true }) recaptchaElement: ElementRef;
   activated:boolean = false ;
-
+  
   ngOnInit() {
+    this.addRecaptchaScript();
   }
-
+  
+  Enter() {
+    this.isLoading = true;
+    this.authService.SignIn(this.email, this.pass).then((result) => {
+      this.isLoading = false;
+    }).catch((error) => {
+      this.isLoading = false;
+      console.info(error);
+    });
+  }
+  
+  Register(){
+    this.isLoading = true;
+    this.authService.SignUp(this.newEmail, this.newPass, this.newName).then((result) => {
+      this.isLoading = false;
+    }).catch((error) => {
+      this.isLoading = false;
+      console.info(error);
+    });
+  }
+  
   rightPanelActive(){
     this.activated = true;
   }
-
+  
   rightPanelNotActive(){
     this.activated = false;
   }
+  
+  addRecaptchaScript() {
+ 
+    window['grecaptchaCallback'] = () => {
+      this.renderReCaptcha();
+    }
+   
+    (function(d, s, id, obj){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) { obj.renderReCaptcha(); return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "https://www.google.com/recaptcha/api.js?onload=grecaptchaCallback&amp;render=explicit";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'recaptcha-jssdk', this));
+   
+  }
 
+  renderReCaptcha() {
+    window['grecaptcha'].render(this.recaptchaElement.nativeElement, {
+      'sitekey' : '6LdVZr8UAAAAAGUuINv5OIeNIoBm1Sh1FltfVneo ',
+      'callback': (response) => {
+          console.log(response);
+      }
+    });
+  }
+  
 }
